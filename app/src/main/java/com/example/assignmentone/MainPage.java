83475d4b7c1e688145a4e601df14723359861421
lbroomf <lawrence.broomfield@gmail.com>
2021-09-21 17:57:37 -0400

    Created the images and the intents

--- app/src/main/java/com/example/assignmentone/MainPage.java
+++ app/src/main/java/com/example/assignmentone/MainPage.java
@@ -1,8 +1,12 @@
 package com.example.assignmentone;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainPage extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        TextView textClick = (TextView) findViewById(R.id.textView);
+        textClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainPage.this, RegistrationPage.class));
+            }
+        });
+
+        ImageButton button = (ImageButton) findViewById(R.id.imageButton3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainPage.this, RegistrationPage.class));
+            }
+        });
+
     }
 }
