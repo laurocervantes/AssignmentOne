7dd32ab524b00dfb49716deaf16189a2861b78d8
lbroomf <lawrence.broomfield@gmail.com>
2021-09-22 21:43:21 -0400

    Created the images and the intents

--- app/src/main/java/com/example/assignmentone/RegistrationPage.java
+++ app/src/main/java/com/example/assignmentone/RegistrationPage.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class RegistrationPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_page);
+
+
     }
 }
