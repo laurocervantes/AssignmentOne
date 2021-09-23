4dee0f7a5b2f915308d7dfa8e1c6b15934193e8c
lbroomf <lawrence.broomfield@gmail.com>
2021-09-23 19:51:10 -0400

    faded

--- app/src/main/java/com/example/assignmentone/RegistrationPage.java
+++ app/src/main/java/com/example/assignmentone/RegistrationPage.java
@@ -5,12 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.Objects;
+
 public class RegistrationPage extends AppCompatActivity {
+
+    private TextInputEditText textInputLayoutEmail;
+    private TextInputEditText textInputLayoutPass;
+    private TextInputEditText textInputLayoutConfirm;
+    private ImageButton imageButton5;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +40,25 @@ public class RegistrationPage extends AppCompatActivity {
                 startActivity(new Intent(RegistrationPage.this, MainPage.class));
             }
         });
+        imageButton5 = (ImageButton) findViewById(R.id.imageButtonFinal);
+        textInputLayoutEmail = (TextInputEditText) findViewById(R.id.email_address);
 
 
+        imageButton5.setVisibility(View.INVISIBLE);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    imageButton5.setVisibility(View.VISIBLE);
+                }
+            } ,5000);
+
+        textView = (TextView) findViewById(R.id.textView28);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(RegistrationPage.this,"THANKS FOR REGISTERING",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
