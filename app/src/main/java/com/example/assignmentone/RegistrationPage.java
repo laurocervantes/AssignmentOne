8a36e2f47822b95fd2d999880309d8b0fa8e3175
lbroomf <lawrence.broomfield@gmail.com>
2021-09-22 23:05:06 -0400

    Second activity sends back to first one

--- app/src/main/java/com/example/assignmentone/RegistrationPage.java
+++ app/src/main/java/com/example/assignmentone/RegistrationPage.java
@@ -2,7 +2,11 @@ package com.example.assignmentone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -12,6 +16,14 @@ public class RegistrationPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_page);
+
+        ImageButton imageButton = (ImageButton) findViewById(R.id.imageButton5);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationPage.this, MainPage.class));
+            }
+        });
 
 
     }
